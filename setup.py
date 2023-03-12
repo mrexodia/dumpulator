ee67b57d81b0131a7afbbd79a96723ad112f4560
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2023-03-12 16:37:33 +0100

    Support [options].develop_requires in setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import subprocess
+import sys
 
 # Based on: https://stackoverflow.com/a/62983901/1806760
 
@@ -21,5 +23,19 @@ if __name__ == "__main__":
                     f.write(line)
         except:
             pass
-
+    # HACK: support [options].develop_requires install development dependencies
+    if "develop" in sys.argv:
+        import configparser
+        config = configparser.ConfigParser()
+        config.read("setup.cfg")
+        if "options" in config:
+            options = config["options"]
+            if "develop_requires" in options:
+                develop_requires = [line for line in options["develop_requires"].splitlines() if line]
+                pip_args = [sys.executable, "-m", "pip", "install", *develop_requires]
+                print(f"Installing development requirements: python " + " ".join(pip_args[1:]))
+                try:
+                    subprocess.check_call(pip_args)
+                except subprocess.CalledProcessError:
+                    sys.exit(1)
     setuptools.setup()
